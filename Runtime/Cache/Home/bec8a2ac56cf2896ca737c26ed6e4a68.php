0f5d0ee35cb04582e05d7ccfc07973ce3fd99ee4
Hennessy.zhao <834819108@qq.com>
2016-08-08 19:23:23 +0800

    8

--- Runtime/Cache/Home/bec8a2ac56cf2896ca737c26ed6e4a68.php
+++ Runtime/Cache/Home/bec8a2ac56cf2896ca737c26ed6e4a68.php
@@ -38,16 +38,72 @@
                     
                         <img src="/alpha/Public/userimg/userimg.jpg" alt=""> <p class="username">Hennessy-zhao</p>
                 </div>
-                <div class="col-md-10" id="usermessage">
-                    爱上对方过后就哭了自行车把你们千万儿童与欧普自行车不能马上的风格和接口里千万儿童与欧普千万儿童与欧普。
+                <div class="col-md-10" id="usercomment">
+                    <div class="col-md-12" id="usermessage">
+                        爱上对方过后就哭了自行车把你们千万儿童与欧普自行车不能马上的风格和接口里千万儿童与欧普千万儿童与欧普。
+                    </div>
+                    <div class="col-md-12" id="others">
+                        <span class="glyphicon glyphicon-thumbs-up"></span>
+                        <span class="good">356</span>
+                        <span class="time">2016-3-4</span>
+                        <span class="lookcomment">收起回复</span>
+                        <span class="reply">回复</span>
+                        <div class="col-md-10 newcomment">
+                            <textarea class="form-control"></textarea>
+                            <button class="btn btn-default btn-sm">提交</button>
+                        </div>
+                        
+                    </div>
+                    <div class="col-md-11" id="secondcomment">
+                        <div class="col-md-12" id="onesecondcomment">
+                            <div class="col-md-1" id="secondimg">
+                                <img src="/alpha/Public/userimg/userimg.jpg" alt="">
+                            </div>
+                            <div class="col-md-10" id="secondmessage">
+                                <p class="message"><span class="username">赵益石</span><span class="replys">回复</span><span class="username">Hennessy-zhao&nbsp;:</span>爱上对方过后就哭了自行车把你们千万儿童与欧普千万儿童与欧普自行车被你骂啥地方规划局可领取儿童</p>
+                            </div>
+                            <div class="col-md-10" id="secondother">
+                                <span class="glyphicon glyphicon-thumbs-up"></span>
+                                <span class="good">356</span>
+                                <span class="time">2016-3-4</span>
+                                <span class="reply">回复</span>
+                                <div class="col-md-11 newcomment">
+                                    <textarea class="form-control"></textarea>
+                                    <button class="btn btn-default btn-sm">提交</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12" id="onesecondcomment">
+                            <div class="col-md-1" id="secondimg">
+                                <img src="/alpha/Public/userimg/userimg.jpg" alt="">
+                            </div>
+                            <div class="col-md-10" id="secondmessage">
+                                <p class="message"><span class="username">赵益石</span><span class="replys">回复</span><span class="username">Hennessy-zhao&nbsp;:</span>爱上对方过后就哭了自行车把你们千万儿童与欧普千万儿童与欧普自行车被你骂啥地方规划局可领取儿童</p>
+                            </div>
+                            <div class="col-md-10" id="secondother">
+                                <span class="glyphicon glyphicon-thumbs-up"></span>
+                                <span class="good">356</span>
+                                <span class="time">2016-3-4</span>
+                                <span class="reply">回复</span>
+                                <div class="col-md-11 newcomment">
+                                    <textarea class="form-control"></textarea>
+                                    <button class="btn btn-default btn-sm">提交</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-10" id="others">
-                    <span class="good">350</span>
-                    <span class="time">2016-3-4</span>
-                    <span class="comment">回复</span>
-                </div>
+                
+                
             </div>
-           
+           <div class="col-md-10" id="newfirstcomment">
+                <h3>发表评论</h3>
+                <form action="<?php echo U('Home/Inward/addnewcomment');?>" name="myForm" method="post">
+                   <textarea class="form-control" required="true" rows="5" placeholder="我想说......"></textarea>
+                   <h4>评论者：&nbsp;&nbsp;<?php echo ($_SESSION['user']); ?></h4>
+                   <button class="btn">发表评论</button>
+               </form>
+           </div>
         </div>
     </div>
     
@@ -71,6 +127,14 @@
   .controller("firstController",['$scope',function($scope){
      
   }])
+</script>
+
+<script>
+    $(function(){
+        $(".reply").on("click",function(){
+            $(this).siblings(".newcomment").toggle()
+        })
+    })
 </script>
 </body>
 </html>
